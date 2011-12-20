b870b7bb75975d93d49195654bebf8c314f1473f
brookzhang <brook.realize@gmail.com>
2011-12-20 17:03:18 +0800

    init a novel site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Novelpool::Application.config.secret_token = '35f69a1ad216bc4bd0b192f333597fe05f55071e9c6ad711cdebaed27eff57cb828809ff58a275d8b9b5adf64b924876df38f84676971b3cae4ad73fccf2a489'
